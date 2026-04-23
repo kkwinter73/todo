552546b5a0bbaea7760ffdbe234472935e35aff9
kkwinter73 <kimura.kota@cail.co.jp>
2026-04-23 11:20:23 +0900

    fc

---
+++ storage.go
@@ -0,0 +1,49 @@
+// TodoListの保存と読み込みを担当する
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+const defaultFilePath = "todos.json"
+
+// ToListをjson変換してファイルに保存する
+func Save(tl *TodoList, filePath string) error {
+	data, err := json.MarshalIndent(tl, "", " ")
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filePath, data, 0644)
+}
+
+// jsonファイル読み込みして返す
+// そのために必要なものは？　ファイルパス
+func Load(filePath string) (*TodoList, error) {
+
+	// ファイル読み込み
+	data, err := os.ReadFile(filePath)
+
+	// ファイルが存在しないかどうかを判定。ファイルが無い場合→初回起動と判定
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return NewTodoList(), nil
+		}
+
+		return nil, err
+	}
+
+	// 空のTodoList変数を宣言
+	var tl TodoList
+
+	// jsonテキストをToListの構造体に変換
+	if err := json.Unmarshal(data, &tl); err != nil {
+		return nil, err
+	}
+
+	// 変換した構造体のポインタを返す（　呼び出し元がこのデータを使う）
+	return &tl, nil
+}
---
+++ todo.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+// 構造体の定義（ TODO そのもの）
+type Todo struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title`
+	Done      bool      `json:"done"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// 構造体の定義　（ 一覧表示したり、タスクを追加したりするようのもの）
+type TodoList struct {
+	Todos  []Todo `json:"Todos"`
+	NextID int    `json:"next_id"`
+}
+
+// 初期化関数（メソッドではない）　レシーバないでしょ
+func NewTodoList() *TodoList {
+	return &TodoList{
+		Todos:  []Todo{}, // 空スライス nil だとjson変換時に "null"になってしまう（型がなくなる）
+		NextID: 1,
+	}
+}
+
+// タスク追加用
+func (tl *TodoList) Add(title string) Todo {
+	todo := Todo{
+		ID:        tl.NextID,
+		Title:     title,
+		Done:      false,
+		CreatedAt: time.Now(),
+	}
+
+	tl.Todos = append(tl.Todos, todo)
+	tl.NextID++
+	return todo
+}
+
+// 一覧表示用
+func (tl *TodoList) List() []Todo {
+	return tl.Todos
+}
+
+// タスク完了反映用
+func (tl *TodoList) Done(id int) error {
+	for i := range tl.Todos {
+		if tl.Todos[i].ID == id {
+			if tl.Todos[i].Done {
+				return fmt.Errorf("タスク %d は既に完了しています", id)
+			}
+			tl.Todos[i].Done = true
+			return nil
+		}
+	}
+	return errors.New("指定されたIDのタスクがありません")
+}
+
+// タスク削除用
+func (tl *TodoList) Delete(id int) error {
+	for i := range tl.Todos {
+		if tl.Todos[i].ID == id {
+			tl.Todos = append(tl.Todos[:i], tl.Todos[i+1:]...)
+			return nil
+		}
+	}
+
+	return errors.New("指定されたIDのタスクがありません")
+}
